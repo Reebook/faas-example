77b37974a17c51668e0464fdda51fd8edeabb30f
David Alvarez <hola@alvarezdavid.com>
2021-11-06 22:34:47 -0600

    Add publisher functions

---
+++ netlify/functions/publisherUpdate.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const clientPromise = require('./mongoDB');
+const headers = require('./headersCORS');
+
+exports.handler = async (event, context) => {
+
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    const client = await clientPromise;
+    const id = parseInt(event.path.split("/").reverse()[0]);
+    const data = JSON.parse(event.body);
+
+    await client.db("Books").collection("Publisher").updateOne({_id:String(id)},{$set:data});
+
+    return { statusCode: 200, headers, body: 'OK'};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 422, headers, body: JSON.stringify(error) };
+  }
+};
